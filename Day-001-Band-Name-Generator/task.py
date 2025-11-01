34116c69b379600945fd4478ce9c1c4683aacbee
Samet ELBEYLİOĞLU <sametelbeylioglu00@gmail.com>
2025-11-01 17:53:31 +0300

    Day 1: Band Name Generator Projesi Tamamlandı

---
+++ Day-001-Band-Name-Generator/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+birthplace = input("Where were you born?\n")
+pet = input("What was your favorite pet?\n")
+
+print(birthplace +" "+ pet + " May your new name be blessed!!!")
